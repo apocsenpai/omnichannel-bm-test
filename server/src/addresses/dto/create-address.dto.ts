dd8829167693d299dfbe56d5ada61039874f1406
apocsenpai <jonatassennas@ufrrj.br>
2023-10-28 17:25:34 -0300

    feat: implements better error messages

--- server/src/addresses/dto/create-address.dto.ts
+++ server/src/addresses/dto/create-address.dto.ts
@@ -8,39 +8,39 @@ import {
 import { Address } from '../interfaces/address.interface';
 
 export class CreateAddressDto implements Address {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O campo Rua não possui caracteres válidos!' })
+  @IsNotEmpty({ message: 'O campo Rua é obrigatório!' })
   @MaxLength(50, { message: 'O campo Rua excedeu o limite de caracteres!' })
   street: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O campo Número não possui caracteres válidos!' })
+  @IsNotEmpty({ message: 'O campo Número é obrigatório!' })
   @MaxLength(7, { message: 'O campo Número excedeu o limite de caracteres!' })
   number: string;
 
   @IsNumberString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'O campo CEP é obrigatório!' })
   @Length(8, 8, { message: 'O campo CEP deve ter 8 caracteres numéricos.' })
   zipCode: string;
 
-  @IsString()
+  @IsString({ message: 'O campo Complemento não possui caracteres válidos!' })
   @MaxLength(50, {
     message: 'O campo Complemento excedeu o limite de caracteres!',
   })
   complement: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O campo Bairro não possui caracteres válidos!' })
+  @IsNotEmpty({ message: 'O campo Bairro é obrigatório!' })
   @MaxLength(50, { message: 'O campo Bairro excedeu o limite de caracteres!' })
   neighborhood: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O campo Cidade não possui caracteres válidos!' })
+  @IsNotEmpty({ message: 'O campo Cidade é obrigatório!' })
   @MaxLength(50, { message: 'O campo Cidade excedeu o limite de caracteres!' })
   city: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O campo Estado não possui caracteres válidos!' })
+  @IsNotEmpty({ message: 'O campo Estado é obrigatório!' })
   @MaxLength(30, { message: 'O campo Estado excedeu o limite de caracteres!' })
   state: string;
 }
--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -15,43 +15,51 @@ import {
 import { CreateAddressDto } from 'src/addresses/dto/create-address.dto';
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({
+    message: 'O campo Nome de usuário não possui caracteres válidos!',
+  })
+  @IsNotEmpty({ message: 'O campo Nome de usuário é obrigatório!' })
   @MaxLength(50, { message: 'Nome de usuário excedeu o limite de caracteres.' })
   username: string;
 
-  @IsString()
+  @IsString({ message: 'O campo Email não possui caracteres válidos!' })
   @IsEmail()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'O campo Email é obrigatório!' })
   @MaxLength(80, {
     message: 'O campo Email excedeu o limite de caracteres.',
   })
   email: string;
 
   @IsString()
-  @IsStrongPassword({
-    minLength: 6,
-  })
-  @IsNotEmpty()
+  @IsStrongPassword(
+    {
+      minLength: 6,
+    },
+    { message: 'A senha não é forte o suficiente!' },
+  )
+  @IsNotEmpty({ message: 'O campo Senha é obrigatório!' })
   password: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O campo Nome não possui caracteres válidos!' })
+  @IsNotEmpty({ message: 'O campo Nome é obrigatório!' })
   @MaxLength(80, {
     message: 'O campo Nome excedeu o limite de caracteres.',
   })
   name: string;
 
-  @IsNumberString()
-  @IsNotEmpty()
+  @IsNumberString({}, { message: 'O campo CPF deve possuir apenas números!' })
+  @IsNotEmpty({ message: 'O campo CPF é obrigatório!' })
   @Length(11, 11, { message: 'O campo CPF deve ter 11 caracteres numéricos.' })
   cpf: string;
 
-  @IsDateString()
-  @IsNotEmpty()
+  @IsDateString(
+    {},
+    { message: 'O campo Data de nascimento não possui caracteres válidos!' },
+  )
+  @IsNotEmpty({ message: 'O campo Data de nascimento é obrigatório!' })
   birthday: Date;
 
-  @IsEnum(Gender)
+  @IsEnum(Gender, { message: 'Opção selecionada não está disponível!' })
   gender: Gender;
 
   @ValidateNested()
