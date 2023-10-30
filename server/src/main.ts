08490f815d2c56a80f2cbd7be8a073944db067d7
apocsenpai <jonatassennas@ufrrj.br>
2023-10-30 18:36:48 -0300

    chore(server): enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,8 @@ import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
